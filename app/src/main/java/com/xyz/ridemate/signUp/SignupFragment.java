d42613daa264d2cbdf8fcf17a02d94c8a7ef62e7
Orny <orny@hms-macbook-pro-3.local>
2022-10-05 23:33:46 +0600

    implement nav graph

--- app/src/main/java/com/xyz/ridemate/signUp/SignupFragment.java
+++ app/src/main/java/com/xyz/ridemate/signUp/SignupFragment.java
@@ -1,15 +1,14 @@
-package com.xyz.ridemate.login;
+package com.xyz.ridemate.signUp;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
@@ -17,30 +16,32 @@ import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.xyz.ridemate.R;
-import com.xyz.ridemate.databinding.LoginFragmentBinding;
+import com.xyz.ridemate.databinding.SignupFragmentBinding;
 import com.xyz.ridemate.signUp.ViewModel.MainViewModel;
+import com.xyz.ridemate.signUp.ViewModel.SignupViewModel;
 
-public class LoginFragment extends Fragment {
+public class SignupFragment extends Fragment {
+
+    private SignupViewModel mViewModel;
+    private MainViewModel mainViewModel;
 
     private NavController navController;
 
-    private LoginFragmentBinding binding;
+    private SignupFragmentBinding binding;
 
-
-    private LoginViewModel mViewModel;
-
-    public static LoginFragment newInstance() {
-        return new LoginFragment();
+    public static SignupFragment newInstance() {
+        return new SignupFragment();
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        binding = DataBindingUtil.inflate(inflater, R.layout.login_fragment, container, false);
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.signup_fragment, container, false);
 
         return binding.getRoot();
     }
 
-    @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
@@ -51,33 +52,18 @@ public class LoginFragment extends Fragment {
         binding.sign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Navigation.findNavController(view).navigate(R.id.action_login2_to_signUp);
+                Navigation.findNavController(view).navigate(R.id.action_signUp_to_login2);
             }
         });
 
-        binding.forgotpassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Navigation.findNavController(view).navigate(R.id.action_login2_to_recoverPassword);
-            }
-        });
-
-        binding.login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Navigation.findNavController(view).navigate(R.id.action_login2_to_homepage);
-            }
-        });
     }
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
-
-
+        mViewModel = new ViewModelProvider(this).get(SignupViewModel.class);
         // TODO: Use the ViewModel
     }
-
 
 }
